53da99d0f219d6ce266ac248cff3a26d57281cac
danocolombo@google.com <GR0mans1212!>
2021-01-01 19:28:23 -0500

    integerated google sign-in and updated header with conditional signin and signout

--- src/App.js
+++ src/App.js
@@ -5,17 +5,40 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
-                <Route path='/signin' component={SignInAndSignUpPage} />
-            </Switch>
-        </div>
-    );
+import { auth } from './firebase/firebase.utils';
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentUser: null,
+        };
+    }
+    unsubscribeFromAuth = null;
+    componentDidMount() {
+        //create an auth listener
+        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({ currentUser: user });
+            console.log(user);
+            console.log('name: ' + user.displayName);
+            console.log('email: ' + user.email);
+            console.log('uid: ' + user.uid);
+        });
+    }
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser} />
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/shop' component={ShopPage} />
+                    <Route path='/signin' component={SignInAndSignUpPage} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,18 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    // apiKey: 'AIzaSyA__nJiwml1-15OU3dm2CIiPG0IYkMMS-o',
-    // authDomain: 'fortsonclothes.firebaseapp.com',
-    // projectId: 'fortsonclothes',
-    // storageBucket: 'fortsonclothes.appspot.com',
-    // messagingSenderId: '385988075698',
-    // appId: '1:385988075698:web:5b7e694aa9c8d5a55a69ab',
-    apiKey: 'AIzaSyDw5iLfdUyipXMNAXRXQ6p4m9uP4w6jAYQ',
-    authDomain: 'fclothes-dfca2.firebaseapp.com',
-    projectId: 'fclothes-dfca2',
-    storageBucket: 'fclothes-dfca2.appspot.com',
-    messagingSenderId: '76138142067',
-    appId: '1:76138142067:web:e40f2e60010c77c1dd4d06',
+    apiKey: 'AIzaSyA__nJiwml1-15OU3dm2CIiPG0IYkMMS-o',
+    authDomain: 'fortsonclothes.firebaseapp.com',
+    projectId: 'fortsonclothes',
+    storageBucket: 'fortsonclothes.appspot.com',
+    messagingSenderId: '385988075698',
+    appId: '1:385988075698:web:5b7e694aa9c8d5a55a69ab',
 };
 firebase.initializeApp(config);
 
