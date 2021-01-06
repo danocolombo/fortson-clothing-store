bc98ee9cb193434a84f4f276d25204bef3146b85
danocolombo@google.com <GR0mans1212!>
2021-01-06 06:21:18 -0500

    added persist to store the session data of our cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'; // need to add persistStore for session
+//for local storage
+import storage from 'redux-persist/lib/storage';
+//import sessionStorage from 'redux-persist/lib/storage/session';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+//following config defines what persist stores. user is handled by firebase, so only will store user
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'],
+};
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
