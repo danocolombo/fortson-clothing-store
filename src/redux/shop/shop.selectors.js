885735838be38869b7edd8436d2fb4e1c60afbda
danocolombo@google.com <GR0mans1212!>
2021-01-07 22:05:53 -0500

    completed basic application

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,24 +1,21 @@
 import memoize from 'lodash.memoize';
 import { createSelector } from 'reselect';
 //used to map the path string for collection to known number is data store
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5,
-};
+
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     (shop) => shop.collections
 );
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
-    createSelector([selectCollections], (collections) =>
-        collections.find(
-            (collection) =>
-                collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-        )
+    createSelector(
+        [selectCollections],
+        (collections) => collections[collectionUrlParam]
     )
 );
