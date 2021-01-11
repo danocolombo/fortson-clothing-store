ef151f3b71a2e55dca709700f7581e0c681e463c
danocolombo@google.com <GR0mans1212!>
2021-01-11 05:29:16 -0500

    fixed debug middleware assignment in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import { persistStore } from 'redux-persist';
 
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [];
-if(process.env.NODE_ENV === 'development'){
-    middlewares.push('logger');
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
